6291ec3b39ba83a22df77ab55794d447a6294107
AlexK <alkremn@gmail.com>
2018-10-03 13:11:42 -0600

    Products added

--- Invent-it/model/Product.cs
+++ Invent-it/model/Product.cs
@@ -8,5 +8,45 @@ namespace Model
 {
     class Product
     {
+
+        public List<Part> AssociatedParts { get; set; } = new List<Part>();
+
+        private int _productId;
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public int InStock { get; set; }
+
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+
+
+        protected Product(int productId, string partName, double price, int inStock, int min, int max)
+        {
+            ProductId = productId;
+            ProductName = partName;
+            Price = price;
+            InStock = inStock;
+            Min = min;
+            Max = max;
+        }
+
+        public int ProductId
+        {
+            get => _productId;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"Invalid ID number");
+                }
+
+                _productId = value;
+            }
+        }
     }
 }
